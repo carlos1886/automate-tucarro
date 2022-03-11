cbb82299469fa1d9485aaf3b2b8bd191a827af2a
Carlos Arizabaleta <carlosmanu1886@gmail.com>
2022-03-11 13:09:27 -0500

    feat: atomate tu carro

---
+++ cypress/integration/tucarro.spec.js
@@ -0,0 +1,6 @@
+describe("realizacion prueba pagina tucarro", () => {
+    it("visita a la pagina tucarro", () => {
+      cy.visit("https://www.tucarro.com.co/");
+      cy.tucarro()
+    });
+  });
